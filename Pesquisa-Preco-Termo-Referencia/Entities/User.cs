53417a8fd121b0648424dfc97c451cb73470bb6e
Alex Santos <alxmath@gmail.com>
2020-08-22 17:19:18 -0300

    Adicionada a entidade User e o FormUser

---
+++ Pesquisa-Preco-Termo-Referencia/Entities/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pesquisa_Preco_Termo_Referencia.Entities
+{
+    class User
+    {
+        public string Nome { get; set; }
+        public string RG { get; set; }
+        public int Cargo { get; set; }
+
+
+        public User()
+        {
+
+        }
+
+        public User(string nome, string rG, int cargo)
+        {
+            Nome = nome;
+            RG = rG;
+            Cargo = cargo;
+        }
+    }
+}
---
+++ Pesquisa-Preco-Termo-Referencia/Forms/FormUser.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Pesquisa_Preco_Termo_Referencia.Forms
+{
+    partial class FormUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtRg = new System.Windows.Forms.TextBox();
+            this.txtCargo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.btnEscolher = new System.Windows.Forms.Button();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.columnNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnRG = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnCargo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdicionar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtCargo);
+            this.groupBox1.Controls.Add(this.txtRg);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(324, 360);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Adicionar Nova Assinatura";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnEscolher);
+            this.groupBox2.Controls.Add(this.dgvUsers);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.DarkCyan;
+            this.groupBox2.Location = new System.Drawing.Point(342, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(635, 360);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Escolher Assinatura";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(27, 56);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(275, 23);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtRg
+            // 
+            this.txtRg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRg.Location = new System.Drawing.Point(27, 103);
+            this.txtRg.Name = "txtRg";
+            this.txtRg.Size = new System.Drawing.Size(275, 23);
+            this.txtRg.TabIndex = 1;
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.txtCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCargo.FormattingEnabled = true;
+            this.txtCargo.Items.AddRange(new object[] {
+            "Agente de Saúde",
+            "Auxiliar Administrativo",
+            "Auxiliar de Enfermagem",
+            "Auxiliar de Saúde",
+            "Auxiliar de Serviços",
+            "Diretor I",
+            "Diretor Técnico de Saúde I",
+            "Diretor Técnico de Saúde II",
+            "Diretor Técnico I",
+            "Oficial de Saúde",
+            "Técnico de Enfermagem"});
+            this.txtCargo.Location = new System.Drawing.Point(27, 151);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(275, 24);
+            this.txtCargo.Sorted = true;
+            this.txtCargo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(24, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "RG";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cargo";
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.AllowUserToAddRows = false;
+            this.dgvUsers.AllowUserToDeleteRows = false;
+            this.dgvUsers.BackgroundColor = System.Drawing.Color.White;
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnNome,
+            this.columnRG,
+            this.columnCargo});
+            this.dgvUsers.Location = new System.Drawing.Point(17, 40);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.ReadOnly = true;
+            this.dgvUsers.RowHeadersVisible = false;
+            this.dgvUsers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgvUsers.Size = new System.Drawing.Size(600, 259);
+            this.dgvUsers.TabIndex = 0;
+            // 
+            // btnEscolher
+            // 
+            this.btnEscolher.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnEscolher.ForeColor = System.Drawing.Color.White;
+            this.btnEscolher.Location = new System.Drawing.Point(387, 317);
+            this.btnEscolher.Name = "btnEscolher";
+            this.btnEscolher.Size = new System.Drawing.Size(230, 37);
+            this.btnEscolher.TabIndex = 1;
+            this.btnEscolher.Text = "Escolher Assinatura Selecionada";
+            this.btnEscolher.UseVisualStyleBackColor = false;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnAdicionar.ForeColor = System.Drawing.Color.White;
+            this.btnAdicionar.Location = new System.Drawing.Point(201, 317);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(101, 37);
+            this.btnAdicionar.TabIndex = 3;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = false;
+            // 
+            // columnNome
+            // 
+            this.columnNome.HeaderText = "Nome";
+            this.columnNome.Name = "columnNome";
+            this.columnNome.ReadOnly = true;
+            this.columnNome.Width = 200;
+            // 
+            // columnRG
+            // 
+            this.columnRG.HeaderText = "RG";
+            this.columnRG.Name = "columnRG";
+            this.columnRG.ReadOnly = true;
+            this.columnRG.Width = 150;
+            // 
+            // columnCargo
+            // 
+            this.columnCargo.HeaderText = "Cargo";
+            this.columnCargo.Name = "columnCargo";
+            this.columnCargo.ReadOnly = true;
+            this.columnCargo.Width = 220;
+            // 
+            // FormUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 386);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormUser";
+            this.Text = "Adicionar/Escolher Assinatura - Núcleo de Compras";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox txtCargo;
+        private System.Windows.Forms.TextBox txtRg;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.Button btnEscolher;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnRG;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnCargo;
+    }
+}
